8f9f09b37a44c47f677ad26bf7c8424f8d6c7d9c
Michael Lück <git@lueckonline.net>
2023-12-06 14:53:07 +0100

    Solved Day 6 Part 2

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -2,15 +2,23 @@ package net.lueckonline.aoc2023.kotlin
 
 class Day6 {
   fun part1(input: List<String>): Long {
+    return getRacesFromInput(input)
+      .map { it.waysToWin() }
+      .fold(1L) { product, factor -> product * factor }
+  }
 
+  fun part2(input: List<String>): Long {
+    return getRacesFromInput(input).map { it.waysToWin() }.first()
+  }
+
+  private fun getRacesFromInput(input: List<String>): Sequence<Race> {
     val numberRegex = Regex("\\d+")
     val times = numberRegex.findAll(input[0]).map { it.value.toLong() }
     val durations = numberRegex.findAll(input[1]).map { it.value.toLong() }.toList()
 
-    return times
-      .mapIndexed { index, time -> Race(time, durations[index]).waysToWin() }
-      .fold(1L) { product, factor -> product * factor }
+    return times.mapIndexed { index, time -> Race(time, durations[index]) }
   }
+
 }
 
 class Race (private val maxTime : Long, private val record : Long) {
--- src/test/kotlin/Day6Test.kt
+++ src/test/kotlin/Day6Test.kt
@@ -1,5 +1,5 @@
 import net.lueckonline.aoc2023.kotlin.Day6
-import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -14,12 +14,22 @@ class Day6Test {
 
   @Test
   fun shouldFindOptionsToWin() {
-    MatcherAssert.assertThat(day.part1(input), `is`(2269432L))
+    assertThat(day.part1(input), `is`(2269432L))
   }
 
   @Test
   fun shouldFindOptionsToWinExample() {
-    MatcherAssert.assertThat(day.part1(inputExample), `is`(288L))
+    assertThat(day.part1(inputExample), `is`(288L))
+  }
+
+  @Test
+  fun shouldFindOptionsToWinSingleRace() {
+    assertThat(day.part2(input_part2), `is`(35865985L))
+  }
+
+  @Test
+  fun shouldFindOptionsToWinSingleRaceExample() {
+    assertThat(day.part2(input_part2_example), `is`(71503L))
   }
 
   val inputExample = """
@@ -31,4 +41,15 @@ class Day6Test {
     Time:        49     78     79     80
     Distance:   298   1185   1066   1181
   """.trimIndent().split("\n").filter { it.isNotBlank() }
+
+  val input_part2_example="""
+    Time:      71530
+    Distance:  940200
+  """.trimIndent().split("\n").filter { it.isNotBlank() }
+
+  val input_part2 ="""
+    Time:        49787980
+    Distance:   298118510661181
+  """.trimIndent().split("\n").filter { it.isNotBlank() }
+
 }
