5e0800d5c44d61c4dc3917c210748d2da7770665
thuri <git@lueckonline.net>
2023-12-12 09:45:47 +0100

    Intermediate Commit

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,12 @@
+package net.lueckonline.aoc2023.kotlin
+
+class Day10(line : String) {
+
+  private var lines: List<String>
+
+  init {
+    this.lines = line.split("\n").filter { it.isNotBlank() }
+  }
+
+
+}
---
+++ src/test/kotlin/Day10Test.kt
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Named
+import org.junit.jupiter.params.provider.Arguments
+import java.util.stream.Stream
+
+class Day10Test {
+
+
+  companion object {
+    @JvmStatic
+    fun part1Input() : Stream<Arguments> {
+      return Stream.of(
+        Arguments.of(Named.of("example", ))
+      )
+    }
+  }
+}
